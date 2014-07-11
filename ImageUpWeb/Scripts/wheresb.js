efb748f87195e9addc55a14ae247a6cd138944ef
stephenwolfman <wolfman@azoi.net>
2014-07-10 23:39:42 -0700

    Update wheresb.js

--- ImageUpWeb/Scripts/wheresb.js
+++ ImageUpWeb/Scripts/wheresb.js
@@ -91,7 +91,9 @@ WheresB.prototype = {
                             //Add Overlay
                             google.maps.event.addListener(marker, 'click', function() {
                                 _gaq.push(['_trackEvent', 'WheresB:Map', 'click', marker.title]);
-    
+                                if(openInfoWindow){
+                                    openInfoWindow.close();
+                                }
                                 infowindow = new google.maps.InfoWindow({
                                     content: contentS
                                 });
